7eddb19327d9f82f52cbb468dd2978375ab92d16
Natip85 <106977567+Natip85@users.noreply.github.com>
2026-01-26 18:06:35 +0200

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { db } from "@ecommerce/db";
+import * as schema from "@ecommerce/db/schema/auth";
+import { env } from "@ecommerce/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
